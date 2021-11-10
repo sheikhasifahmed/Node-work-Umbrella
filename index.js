531ccc68111ef504907193ca754ed98b29d39f40
sheikasifahmed <sheikhasifahmed100@gmail.com>
2021-11-11 01:48:47 +0600

    port capital

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello...hello.. hello... Everyone to the final Project");

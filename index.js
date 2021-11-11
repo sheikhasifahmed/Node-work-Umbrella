09288cfbb9be51a0a4dd734b05bc9a0c8b5905ce
sheikasifahmed <sheikhasifahmed100@gmail.com>
2021-11-11 20:49:11 +0600

    orders

--- index.js
+++ index.js
@@ -75,10 +75,73 @@ async function run() {
     // });
 
     // deletation of a prouduct
-    app.delete("/product/:id", async (req, res) => {
+    // app.delete("/product/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await productsCollection.deleteOne(query);
+    //   console.log(result);
+    //   res.json(result);
+    // });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+async function order() {
+  try {
+    await client.connect();
+    const database = client.db("umbrella");
+    const ordersCollection = database.collection("orders");
+    // create a document to insert
+
+    app.post("/purchase", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.json(order);
+    });
+
+    app.get("/orders", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    app.get("/my-orders/:user", async (req, res) => {
+      const user = req.params.user;
+      console.log(user);
+      const query = { userEmail: user };
+      console.log(query);
+      const cursor = ordersCollection.find(query);
+      // console.log(cursor);
+      const bookings = await cursor.toArray();
+      console.log(bookings);
+      res.send(bookings);
+    });
+
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const update = req.body;
+      console.log(update);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: { status: update.status },
+      };
+      const result = await ordersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(result);
+      res.json(result);
+    });
+
+    app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const result = await productsCollection.deleteOne(query);
+      const result = await ordersCollection.deleteOne(query);
       console.log(result);
       res.json(result);
     });
@@ -86,4 +149,4 @@ async function run() {
     // await client.close();
   }
 }
-run().catch(console.dir);
+order().catch(console.dir);
